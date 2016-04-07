4a9c08c354415a94a4377402134c0e5ce3d77b97
Will Howell <will.s.howell@gmail.com>
2016-04-06 22:27:06 -0400

    Route index file separately from api routes

--- app.js
+++ app.js
@@ -28,22 +28,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Create router for express
-var router = express.Router();
+// Create routers for express
+var apiRouter = express.Router();
+var interfaceRouter = express.Router();
 
 // Endpoint handlers for /projects
-router.route('/projects')
+apiRouter.route('/projects')
 	.post(projectController.postProjects)
 	.get(projectController.getProjects);
 
 // Endpoint handlers for /projects/:project_id
-router.route('/projects/:project_id')
+apiRouter.route('/projects/:project_id')
 	.get(projectController.getProject)
 	.put(projectController.putProject)
 	.delete(projectController.deleteProject)
 
-// Register all the routes to start with /api
-app.use('/api/v1', router);
+// Endpoint handler for /
+interfaceRouter.get('/', function(req, res) {
+	res.render('index', { title: 'Portfolio CMS' });
+});
+
+// Register all the routes (api routes start with /api/v1/)
+app.use('/api/v1', apiRouter);
+app.use('/', interfaceRouter);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -71,7 +78,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
-		message: err.message;
+		message: err.message,
 		error: {}
 	});
 });
