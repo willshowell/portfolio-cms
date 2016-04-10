75b11f635bef88aae5eedc15eec52ea218e9e3cf
Will Howell <will.s.howell@gmail.com>
2016-04-10 10:56:04 -0400

    Add more fields to projects schema

--- models/project.js
+++ models/project.js
@@ -5,7 +5,17 @@ var mongoose = require('mongoose');
 var ProjectSchema = new mongoose.Schema({
 	name: String,
 	tagline: String,
-	hero_url: String
+	hero_url: String,
+	description: String,
+	source_url: String,
+	project_url: String,
+	images: [String]	
+},
+{
+	timestamps: {
+		createdAt: 'created_at',
+		updatedAt: 'updated_at'
+	}
 });
 
 // Export the model
