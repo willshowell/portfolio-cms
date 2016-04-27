14e917367473c99f3953ec9a193dec0087ba1ece
Will Howell <will.s.howell@gmail.com>
2016-04-26 20:52:23 -0400

    Add prototype routes for application. Prepend /username/ to api routes

--- app.js
+++ app.js
@@ -9,28 +9,24 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 
 // Import controllers
-var projectController = require('./controllers/project');
-
-
-//test new User controller
 var userController = require('./controllers/user');
-
+var projectController = require('./controllers/project');
 var blogPostController = require('./controllers/blogPost');
 
 // Create express application
 var app = express();
 
-//initialize passport
+// Initialize passport
 app.use(passport.initialize());
 
 // Connect to the CMS MongoDB
+// (switch to an alternate DB when testing)
 console.log(app.settings.env);
 var mongoURI = 'mongodb://localhost:27017/cms';
 if (process.env.NODE_ENV === 'test') {
 	mongoURI = 'mongodb://localhost:27017/cms-testing';
 }
 mongoose.connect(mongoURI);
-
 
 // Setup view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -46,20 +42,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Create routers for express
 var apiRouter = express.Router();
-var interfaceRouter = express.Router();
+var applicationRouter = express.Router();
 
+/* 
+ * API Routes
+*/
 // Endpoint handlers for /projects
-apiRouter.route('/projects')
+apiRouter.route('/:username/projects')
 	.post(projectController.postProjects)
 	.get(projectController.getProjects);
 
 // Endpoint handlers for /projects/:project_id
-apiRouter.route('/projects/:project_id')
+apiRouter.route('/:username/projects/:project_id')
 	.get(projectController.getProject)
 	.put(projectController.putProject)
 	.delete(projectController.deleteProject);
 
-	
 // Endpoint handlers for /blogposts
 apiRouter.route('/blogposts')
 	.post(blogPostController.postBlogPosts)
@@ -71,15 +69,52 @@ apiRouter.route('/blogposts/:blogpost_id')
 	.put(blogPostController.putBlogPost)
 	.delete(blogPostController.deleteBlogPost);
 
+/* 
+ * Application Routes 
+*/
 // Endpoint handler for /
-
-interfaceRouter.get('/', function(req, res) {
+applicationRouter.get('/', function(req, res) {
+	// TODO redirect to /:username or /:login depending on session
 	res.render('index', { title: 'Portfolio CMS' });
+});
+
+// Endpoint handler for /login
+applicationRouter.get('/login', function(req, res) {
+	res.send('This is where you will log in');
+});
+
+// Endpoint handler for /signup
+applicationRouter.get('/signup', function(req, res) {
+	res.send('This is where you will sign up');
+});
+
+// Endpoint handler for /:username
+applicationRouter.get('/:username', function(req, res) {
+	res.send('Your username is ' + req.params.username);
+});
+
+// Endpoint handler for /:username/settings
+applicationRouter.get('/:username/settings', function(req, res) {
+	res.send('Here are ' + req.params.username + '\'s settings');
+});
+
+// Endpoint handler for /:username/projects
+applicationRouter.get('/:username/projects', function(req, res) {
+	res.send('Your username is ' + req.params.username + ' and here are your projects');
+});
+
+// Endpoint handler for /:username/projects/new
+applicationRouter.get('/:username/projects/new', function(req, res) {
+	res.send('Making a new project');
+});
+
+applicationRouter.get('/:username/projects/:id', function(req, res) {
+	res.send('Project: ' + req.params.id);
 });
 
 // Register all the routes (api routes start with /api/v1/)
 app.use('/api/v1', apiRouter);
-app.use('/', interfaceRouter);
+app.use('/', applicationRouter);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -88,8 +123,9 @@ app.use(function(req, res, next) {
 	next(err);
 });
 
-// Error handlers
-
+/*
+ * Error handlers
+*/
 // Dev error handler
 // Print stacktrace
 if (app.get('env') === 'development') {
